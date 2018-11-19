0e5c1a19c9653f0f692586b0fc25a7dd5b74d6d8
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-19 17:04:14 +0530

    Initial commit

---
+++ app/src/main/java/fragment/Price.java
@@ -0,0 +1,28 @@
+package fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vaibhav.productdetails.R;
+
+
+/**
+ * Created by wolfsoft4 on 18/8/18.
+ */
+
+public class Price extends Fragment {
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.price,container,false);
+        return view;
+    }
+}
